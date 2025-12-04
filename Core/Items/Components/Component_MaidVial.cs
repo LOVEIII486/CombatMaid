50a09bf9950609ae0681bdc76f8d1f55fddc5d4f
Setekh <setekh4164@gmail.com>
2025-12-04 22:03:50 +0800

    新增瓶中女仆-I

---
+++ Core/Items/Components/Component_MaidVial.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using ItemStatsSystem;
+using System.Linq;
+using CombatMaid.Core;
+
+namespace CombatMaid.Core.Items.Components
+{
+    public class Component_MaidVial : MonoBehaviour
+    {
+        private Item _item;
+        
+        private const string DefaultProfile = "VialMaid_I"; 
+
+        private void Awake()
+        {
+            _item = GetComponent<Item>();
+            if (_item != null) _item.onUse += OnUseVial;
+        }
+
+        private void OnUseVial(Item item, object user)
+        {
+            var player = user as CharacterMainControl;
+            if (player == null) return;
+            
+            string targetProfile = GetProfileFromItem(item);
+            
+            Vector3 spawnPos = player.transform.position + player.transform.forward * 2.0f + Vector3.up * 0.5f;
+            
+            if (MaidManager.Instance != null)
+            {
+                MaidManager.Instance.SpawnMaidAt(targetProfile, spawnPos);
+                player.PopText($"瓶中女仆 [{targetProfile}] 已就绪！");
+            }
+            
+            //item.DestroyTree();
+        }
+
+        private string GetProfileFromItem(Item item)
+        {
+            if (item.Constants != null)
+            {
+                var data = item.Constants.FirstOrDefault(x => x.Key == "MaidProfileID");
+                if (data != null && !string.IsNullOrEmpty(data.GetString()))
+                {
+                    return data.GetString();
+                }
+            }
+            return DefaultProfile;
+        }
+
+        private void OnDestroy()
+        {
+            if (_item != null) _item.onUse -= OnUseVial;
+        }
+    }
+}
--- Core/Items/Data/MaidItemDefs.cs
+++ Core/Items/Data/MaidItemDefs.cs
@@ -38,6 +38,41 @@ namespace CombatMaid.Core.Items.Data
                     ShopPriceFactor = 1.2f,
                     ShopPossibility = 1f,
                     ShopForceUnlock = true
+                },
+                new MaidItemInfo
+                {
+                    itemId = 88889,
+                    spritePath = "VialMaid_I.png",
+    
+                    localizationKey = "Item_VialMaid_I",
+                    localizationDesc = "Item_VialMaid_I_Desc",
+    
+                    value = 2000,
+                    maxStackCount = 5,
+                    weight = 0.5f,
+                    tags = new List<string> { "Maid", "Consumable" },
+
+                    usages = new UsageData
+                    {
+                        useTime = 0.5f,
+                        // useSound = "GlassBreak",
+                        behaviors = new List<UsageBehaviorData>()
+                    },
+
+    
+                    VisualReferenceId = 429,
+                    CustomComponentType = typeof(Component_MaidVial), 
+                    
+                    CustomConstants = new Dictionary<string, object>
+                    {
+                        { "MaidProfileID", "VialMaid_I" }, 
+                        { "ConsumeOnUse", true }
+                    },
+                    
+                    ShopMerchantId = MerchantIds.Mud,
+                    ShopMaxStock = 10,
+                    ShopPriceFactor = 1.5f,
+                    ShopPossibility = 1.0f
                 }
             };
         }
